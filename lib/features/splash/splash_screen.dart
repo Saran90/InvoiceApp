988627d301f14f38778cb11f09c71583f4fc38d8
Saran <saran.mampatta90@gmail.com>
2025-09-04 15:57:39 +0530

    fix: Splash screen ui updated

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
 import '../../utils/routes.dart';
@@ -23,6 +24,39 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Invoice App')));
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(3, 108, 173, 1),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Center(
+              child: SvgPicture.asset(
+                'assets/images/im_icon.svg',
+                width: 307,
+                height: 97,
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text('Powered by', style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white
+                  ),),
+                  Text('Zerosnap ID Solutions Pvt. Ltd.', style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white
+                  )),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
